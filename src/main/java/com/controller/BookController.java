b5bcc6966086da0746f8df3fa7fc4a34d2a4aeb1
Tejas Shah <tejasshah2k19@gmail.com>
2025-09-14 12:50:54 +0530

    update api , ResponseEntity

--- src/main/java/com/controller/BookController.java
+++ src/main/java/com/controller/BookController.java
@@ -1,13 +1,17 @@
 package com.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,10 +25,10 @@ public class BookController {
 	BookRepository bookRepository;
 
 	@PostMapping("/books")
-	public BookEntity addBook(@RequestBody BookEntity bookEntity) {
+	public ResponseEntity<BookEntity> addBook(@RequestBody BookEntity bookEntity) {
 		bookRepository.save(bookEntity);// insert
 		System.out.println(bookEntity);
-		return bookEntity;
+		return ResponseEntity.status(HttpStatus.CREATED).body(bookEntity);
 	}
 
 	// read all books
@@ -37,26 +41,51 @@ public class BookController {
 	// http://localhost:9999/books/3
 	// read single book -> id
 	@GetMapping("/books/{bookId}")
-	public BookEntity getBookById(@PathVariable Integer bookId) {
+	public ResponseEntity<BookEntity> getBookById(@PathVariable Integer bookId) {
 		Optional<BookEntity> op = bookRepository.findById(bookId);
 
 		if (op.isEmpty()) {
-			return null;
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
 		} else {
-			return op.get();
+			return ResponseEntity.status(HttpStatus.OK).body(op.get());
 		}
 	}
 
 	// delete book -> id
 	@DeleteMapping("/books/{bookId}")
-	public BookEntity removeBookById(@PathVariable Integer bookId) {
+	public ResponseEntity<?> removeBookById(@PathVariable Integer bookId) {
 		Optional<BookEntity> op = bookRepository.findById(bookId);
+		HashMap<String, Object> map = new HashMap<>();
+		
+		if (op.isEmpty()) {
+			map.put("msg", "Invalid Id");
+			map.put("bookId", bookId);
 
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
+		} else {
+			map.put("data", op.get());
+			map.put("msg", "success");
+			map.put("code", 1);
+			bookRepository.deleteById(bookId);
+			return ResponseEntity.status(HttpStatus.OK).body(map);
+		}
+	}
+
+	// update
+	@PutMapping("/books/{bookId}")
+	public BookEntity updateBook(@PathVariable Integer bookId, @RequestBody BookEntity bookEntity) {
+		Optional<BookEntity> op = bookRepository.findById(bookId);
 		if (op.isEmpty()) {
 			return null;
 		} else {
-			bookRepository.deleteById(bookId);
-			return op.get();
+			// update
+			BookEntity dbBook = op.get();
+			dbBook.setPrice(bookEntity.getPrice());
+			dbBook.setAuthorName(bookEntity.getAuthorName());
+
+			bookRepository.save(dbBook);// save -> update -> id ?
+			return dbBook;
+
 		}
 
 	}
